197e7c4de617318f26d0f1783504a9f825b41a7d
AnnKrn <anakrn.carreon@gmail.com>
2018-02-14 22:54:25 -0600

    funcion boton large

--- js/app.js
+++ js/app.js
@@ -1,15 +1,29 @@
 var textArea = document.getElementById("user_comment");
 textArea.addEventListener('keyup', paintText)
-// function loadPage() {
-//     $('#user_comment').keyup(paintText)
-// }
 
-// funcion de preview
+// function loadPage() {
+    //     $('#user_comment').keyup(paintText)
+    // }
+    
+    // funcion de preview
 function paintText() {
     var $textInput = $('#user_comment');
     var $inputVal = $textInput.val();
-    // console.log($inputVal)
-    $('.coment').html($inputVal)
+        // console.log($inputVal)
+   $('.coment').html($inputVal)
+}
+
+// Botón para hacer más grande la letra
+var largeBtn = document.getElementById("large");
+    // console.log(largeBtn)
+largeBtn.addEventListener("click", changeLarge)
+
+function changeLarge() {
+    // console.log('hola')
+    var textToChange = document.getElementsByClassName("coment").value;
+    console.log(textToChange)
+    textToChange.style.fontsize = "xx-large"
+    
 }
 
 // $(document).ready(loadPage);
